09f7f15d37381ad12e80dc94d31d4c2c3815bf1e
Bas Hendriks <bbehendriks@gmail.com>
2018-03-02 16:35:22 +0100

    added styling and images and added some extra stylesheets

--- public/js/app.js
+++ public/js/app.js
@@ -13597,7 +13597,6 @@ module.exports = __webpack_require__(36);
 /* 11 */
 /***/ (function(module, exports, __webpack_require__) {
 
-
 /**
  * First we will load all of this project's JavaScript dependencies which
  * includes Vue and other libraries. It is a great starting point when
@@ -13606,11 +13605,30 @@ module.exports = __webpack_require__(36);
 
 __webpack_require__(12);
 
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
+$(document).ready(function () {
+    // Apply active class when hamburger is clicked
+    var $hamburger = $(".hamburger");
+    $hamburger.click(function (e) {
+        $hamburger.toggleClass("is-active");
+    });
+
+    // Slide animation for menu
+    $('#menu').on('show.bs.dropdown', function (e) {
+        $(this).find('.dropdown-menu').first().stop(true, true).slideDown(300);
+    });
+
+    // Remove active class from hamburger when menu is closed and add slide animation
+    $('#menu').on('hide.bs.dropdown', function () {
+        $hamburger.removeClass("is-active");
+        $(this).find('.dropdown-menu').first().stop(true, true).slideUp(200);
+    });
+
+    // Parallax effect for home header image
+    $(window).scroll(function () {
+        var st = $(this).scrollTop();
+        $(' .header-image').css({ 'background-position': 'center calc(50% + ' + st * 0.7 + 'px)' });
+    });
+});
 
 /***/ }),
 /* 12 */
--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,4 +1,3 @@
-
 /**
  * First we will load all of this project's JavaScript dependencies which
  * includes Vue and other libraries. It is a great starting point when
@@ -7,10 +6,28 @@
 
 require('./bootstrap');
 
+$(document).ready(function () {
+    // Apply active class when hamburger is clicked
+    var $hamburger = $(".hamburger");
+    $hamburger.click(function (e) {
+        $hamburger.toggleClass("is-active");
+    });
 
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
+    // Slide animation for menu
+    $('#menu').on('show.bs.dropdown', function(e){
+        $(this).find('.dropdown-menu').first().stop(true, true).slideDown(300);
+    });
+
+    // Remove active class from hamburger when menu is closed and add slide animation
+    $('#menu').on('hide.bs.dropdown', function () {
+        $hamburger.removeClass("is-active");
+        $(this).find('.dropdown-menu').first().stop(true, true).slideUp(200);
+    });
+
+    // Parallax effect for home header image
+    $(window).scroll(function () {
+        var st = $(this).scrollTop();
+        $(' .header-image').css({'background-position': 'center calc(50% + ' + (st * 0.7) + 'px)'});
+    });
+});
 
